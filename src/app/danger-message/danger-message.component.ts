f8f295cf8ea17795dc76d45b2d9522e15657d627
torres10luis <Luis.angel_15@yahoo.com>
2018-06-05 14:27:09 -0700

    final fixes to code in practice components

--- src/app/danger-message/danger-message.component.ts
+++ src/app/danger-message/danger-message.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector: 'app-danger-message',
+  selector: './app-danger-message',
   templateUrl: './danger-message.component.html',
   styleUrls: ['./danger-message.component.css']
 })
