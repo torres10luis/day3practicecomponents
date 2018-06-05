5a06e1ba19bcf8fcde02f746b533c834e92346a1
torres10luis <Luis.angel_15@yahoo.com>
2018-06-05 15:27:46 -0700

    final fixes to code in practice components

--- src/app/warning-message/warning-message.component.ts
+++ src/app/warning-message/warning-message.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-warning-message',
   template: `
-    <h3>my warning message is successful </h3>
+    <h2> warning message </h2>
   `,
-  styles: [`
-    h3{
-      color: red;
-    }
-  `]
+  styleUrls: ['./warning-message.component.css']
 })
 export class WarningMessageComponent implements OnInit {
 
